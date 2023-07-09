8effb23f3ee5c57d9bcb0f2d431e1f6068b224f8
K.Utsunomiya <kii.utsunomiya@gmail.com>
2023-07-09 23:44:37 +0900

    example下のgatsbyプロジェクトにて、クエリ実行結果内容を確認できるようにする

--- example/gatsby-v4/src/pages/index.tsx
+++ example/gatsby-v4/src/pages/index.tsx
@@ -1,8 +1,49 @@
-import type { PageProps } from 'gatsby'
-import type { FC } from 'react'
+import type { PageProps } from 'gatsby';
+import type { FC } from 'react';
+import { graphql } from 'gatsby';
 
-const HomePage: FC<PageProps> = () => {
-  return <main>It works!</main>
-}
+const HomePage: FC<PageProps> = ({ data }) => {
+  return (
+    <main>
+      <pre>
+        <code>{JSON.stringify(data, null, 2)}</code>
+      </pre>
+    </main>
+  );
+};
 
-export default HomePage
+export const query = graphql`
+  query HomePageQuery {
+    allMicrocmsPosts(sort: { fields: publishedAt, order: DESC }) {
+      nodes {
+        id
+        createdAt
+        updatedAt
+        publishedAt
+        revisedAt
+
+        sortIndex
+        postsId
+
+        internal {
+          content
+        }
+      }
+    }
+    allMicrocmsSetting(sort: { fields: publishedAt, order: DESC }) {
+      nodes {
+        id
+        createdAt
+        updatedAt
+        publishedAt
+        revisedAt
+
+        internal {
+          content
+        }
+      }
+    }
+  }
+`;
+
+export default HomePage;
